5aa3101b6e4cec950f041b279415b145800f40e9
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-06 20:13:50 +0200

    Implement code changes to enhance functionality and improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 function App() {
   return (
-    <>
-      <div>hi</div>
-    </>
-  );
+    <div className="mx-auto w-150 border-2 border-red-900">
+      <header className="flex h-42 flex-col justify-center gap-5 bg-[url('/burger.jpg')] bg-cover bg-center px-12 text-white">
+        <h1 className="text-5xl font-normal">Jimmy's Diner</h1>
+        <p className="text-xl font-normal">
+          The best burgers and pizzas in town.
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
